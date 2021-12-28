79e404faf318626c2d37eafc793c2debc48f3c5f
vika-Kov <74508958+vika-Kov@users.noreply.github.com>
2021-12-28 08:43:46 +0300

    renderSearchFormBlock - доработка добавлены аргументы, вычисляюся минимальная и максимальная даты, поля заполняются по умолчанию

--- project-template/src/index.ts
+++ project-template/src/index.ts
@@ -1,14 +1,22 @@
-import { renderSearchFormBlock } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock } from './user.js'
-import { renderToast } from './lib.js'
+import { renderSearchFormBlock } from "./search-form.js";
+import { renderSearchStubBlock } from "./search-results.js";
+import { renderUserBlock } from "./user.js";
+import { renderToast } from "./lib.js";
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('Vika Ivanova','./img/avatar.png', 4)
-  renderSearchFormBlock()
-  renderSearchStubBlock()
+window.addEventListener("DOMContentLoaded", () => {
+  renderUserBlock("Ivan Ivanovich", "./img/avatar.png", 0);
+  renderSearchFormBlock();
+  renderSearchStubBlock();
   renderToast(
-      {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
-      {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
-  )
-})
+    {
+      text: "Это пример уведомления. Используйте его при необходимости",
+      type: "success",
+    },
+    {
+      name: "Понял",
+      handler: () => {
+        console.log("Уведомление закрыто");
+      },
+    }
+  );
+});
--- project-template/src/user.ts
+++ project-template/src/user.ts
@@ -1,22 +1,29 @@
-import { renderBlock } from './lib.js'
+import { renderBlock } from "./lib.js";
 
-export function renderUserBlock (userName: string, avatar: string, favoriteItemsAmount:number) {
-  const favoritesCaption = favoriteItemsAmount > 0 ? favoriteItemsAmount : 'ничего нет'
-  const hasFavoriteItems = favoriteItemsAmount > 0 ? true : false
+export function renderUserBlock(
+  userName: string,
+  avatar: string,
+  favoriteItemsAmount: number
+) {
+  const favoritesCaption =
+    favoriteItemsAmount > 0 ? favoriteItemsAmount : "ничего нет";
+  const hasFavoriteItems = favoriteItemsAmount > 0;
 
   renderBlock(
-    'user-block',
+    "user-block",
     `
     <div class="header-container">
       <img class="avatar" src="${avatar}" alt="Wade Warren" />
       <div class="info">
           <p class="name">${userName}</p>
           <p class="fav">
-            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+            <i class="heart-icon${
+              hasFavoriteItems ? " active" : ""
+            }"></i>${favoritesCaption}
           </p>
       </div>
     </div>
     `
-  )
+  );
 }
 /*Найдите функцию renderUserBlock и доработайте её следующим образом. Функция должна принимать три аргумента имя пользователя, ссылка на его аватар и количество элементов в избранном. Аргументы должны иметь подходящие для них типы. Аргументы должны использоваться для соответствующих целей. Убедиться, что следующая логика работает верно, а если нет, то внести правки. Если количество избранных объектов меньше одного, то нужно выводить сообщение “ничего нет” и иконка сердечка должны быть не закрашена. Иначе иконка сердечка закрашена и рядом просто выводится количество избранных объектов. Все стили уже присутствуют, необходима только логика.*/
